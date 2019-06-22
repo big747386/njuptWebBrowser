d1faee798a940060ae8c4f6a10500c00be6a5cfb
yuyim <yuyimiaowu@outlook.com>
2019-06-22 10:28:14 +0800

    android init feat 显示html网页

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.app;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = findViewById(R.id.activity_main_webview);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new MyWebViewClient());
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        //见图片调整到适合webview的大小
+        webSettings.setUseWideViewPort(true);
+        //缩放至屏幕的大小
+        webSettings.setLoadWithOverviewMode(true);
+
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+        //开启http和https混用
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
+        }
+
+        // REMOTE RESOURCE
+        mWebView.loadUrl("http://www.oneplusbbs.com/?adid=qqbrowser_103\n");
+        //mWebView.setWebViewClient(new MyWebViewClient());
+
+        // LOCAL RESOURCE
+        // mWebView.loadUrl("file:///android_asset/index.html");
+    }
+
+    // Prevent the back-button from closing the app
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mWebView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mWebView != null) {
+            //System.out.println(mWebView.toString());
+            mWebView.loadDataWithBaseURL(null,"",
+                    "text/html","utf-8", null);
+
+            mWebView.clearHistory();
+            ((ViewGroup) mWebView.getParent()).removeView(mWebView);
+            mWebView.destroy();
+            mWebView = null;
+        }
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/example/app/MyWebViewClient.java
@@ -0,0 +1,32 @@
+package com.example.app;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+class MyWebViewClient extends WebViewClient {
+
+    //@SuppressWarnings("deprecation")
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        /*Uri uri = Uri.parse(url);
+        if (uri.getHost() != null && uri.getHost().contains("njupt.edu.cn/")) {
+            return false;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        view.getContext().startActivity(intent);
+        return true;*/
+        view.loadUrl(url);
+        return true;
+    }
+
+    public MyWebViewClient() {
+        super();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            
+        }
+    }
+}
